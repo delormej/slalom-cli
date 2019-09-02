3a1796f9407075f66296e036da9f790886a5135c
jjdelorme <jjdelorme@yahoo.com>
2019-09-01 20:50:39 -0400

    Updating version of slalomtracker and adding some debug files.

--- build.sh
+++ build.sh
@@ -1,16 +1,13 @@
-#!/bin/bash
-#VSS_NUGET_EXTERNAL_FEED_ENDPOINTS="{'endpointCredentials': [{'endpoint':'https://pkgs.dev.azure.com/jasondel/_packaging/SlalomTracker/nuget/v3/index.json', 'password':'zdkrbk4vjy4pficjrgycqkwrjwdmjnpx5lisyqc7upo3pug7jwoa'}]}"
-#docker build -t skiconsole -f Dockerfile --build-arg nuget=\"$VSS_NUGET_EXTERNAL_FEED_ENDPOINTS\" .
-
 #
 # This file used for DEBUG build
 #
 rm -rf ref
 mkdir -p ref/ 
 cp -r ../SlalomTracker/bin/Debug/* ./ref/
-docker build -t skiconsole -f Dockerfile .
+docker build -t skiconsole -f debug.Dockerfile .
 #
 # Launch debug container
 #
-SKIBLOBS="DefaultEndpointsProtocol=https;AccountName=skivideostorage;AccountKey=74gV///fVtd/ZL+PzXZU6nsOVzIvt6XC59T9elFnY91vCVqmitlHxNA9QLbQsedTmnCzSR0BhtL0J8dwOVSWvA==;EndpointSuffix=core.windows.net"
+# Need to input env variable value here:
+#SKIBLOBS=
 docker run -it --rm -e SKIBLOBS=\"$SKIBLOBS\" --name ski-dbg skiconsole /bin/bash
